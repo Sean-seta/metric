199c82ea5497844a8ddcc00519bb53aa2bda9b98
Sean <sle@veritone.com>
2025-07-24 11:09:08 +0700

    add log and fix grafana

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/zsais/go-gin-prometheus"
 	"math/rand"
@@ -11,9 +12,9 @@ func genRandom() time.Duration {
 	rand.Seed(time.Now().UnixNano())
 
 	// Generate a random number between 1 and 5
-	randomNumber := rand.Intn(5) + 1 // 1 to 5
+	randomNumber := rand.Intn(300) + 1 // 1 to 300
 
-	return time.Duration(randomNumber) * time.Second
+	return time.Duration(randomNumber) * time.Millisecond
 }
 
 func main() {
@@ -26,20 +27,29 @@ func main() {
 
 	p.Use(r)
 	r.GET("/", func(c *gin.Context) {
+		start := time.Now()
 		time.Sleep(genRandom())
 		c.JSON(200, "Hello world!")
+		end := time.Now()
+		fmt.Printf("Path: /, Request duration: %f seconds\n", end.Sub(start).Seconds())
 	})
 	r.GET("/ping", func(c *gin.Context) {
+		start := time.Now()
 		time.Sleep(genRandom())
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+		end := time.Now()
+		fmt.Printf("Path: /ping, Request duration: %f seconds\n", end.Sub(start).Seconds())
 	})
 	r.GET("/health", func(c *gin.Context) {
+		start := time.Now()
 		time.Sleep(genRandom())
 		c.JSON(200, gin.H{
 			"status": "ok",
 		})
+		end := time.Now()
+		fmt.Printf("Path: /health, Request duration: %f seconds\n", end.Sub(start).Seconds())
 	})
 	r.Run(":8080")
 }
